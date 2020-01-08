c9c9f370a8481b9f815597a133106dbbe6ea18a3
sharpshooter5610 <49451931+sharpshooter5610@users.noreply.github.com>
2020-01-08 22:27:08 +0530

    added basic features

---
+++ src/services/fakeMovieService.js
@@ -0,0 +1,35 @@
+import * as genresAPI from "./fakeGenreService";
+const axios=require("axios");
+let movies=[];
+const api="http://localhost:3000/api";
+export async function getMovies() {
+  movies=await axios.get(api+"/movies");
+ return movies;
+}
+
+export async function getMovie(id) {
+  let movie=await axios.get(api+"/movies/"+id);
+  return movie;
+}
+
+export function saveMovie(movie) {
+  console.log(movie);
+  let movieInDb = movies.find(m => m._id === movie._id) || {};
+  movieInDb.title = movie.title;
+  movieInDb.genre = genresAPI.genres.find(g => g._id === movie.genreId);
+  movieInDb.numberInStock = movie.numberInStock;
+  movieInDb.dailyRentalRate = movie.dailyRentalRate;
+
+  if (!movieInDb._id) {
+    movieInDb._id = Date.now().toString();
+    movies.push(movieInDb);
+  }
+
+  return movieInDb;
+}
+
+export function deleteMovie(id) {
+  let movieInDb = movies.find(m => m._id === id);
+  movies.splice(movies.indexOf(movieInDb), 1);
+  return movieInDb;
+}
